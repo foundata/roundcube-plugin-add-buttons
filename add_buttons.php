8b15c2747844a9996cdbe396519722c4d60de1f3
Andreas Haerter <ah@foundata.com>
2024-01-08 02:10:22 +0100

    Adapt filenames, SVG experiment for RC+ skin

--- add_buttons.php
+++ add_buttons.php
@@ -20,7 +20,7 @@ class add_buttons extends rcube_plugin
         $this->load_config();
 
         // load skin-specific stylesheets (if any)
-        $stylesheet_path = $this->local_skin_path() . '/add_button.css';
+        $stylesheet_path = $this->local_skin_path() . '/add_buttons.css';
         //if (file_exists($stylesheet_path)) {
             $this->include_stylesheet($stylesheet_path);
         //}
@@ -54,13 +54,13 @@ class add_buttons extends rcube_plugin
                 continue;
             }
 
-            $content = $button_item['label'];
 
-            /* FIXME To be implemented
             // userdefined inline SVG icon
             if (isset($button_item['icon_inline']) && $button_item['icon_inline'] !== '') {
-                // FIXME
+                // FIXME check for SVG
+                $content = $button_item['icon_inline'];
 
+            /* FIXME To be implemented
             // Heroicons v2 icon
             } elseif (isset($button_item['icon_heroicons_v2']) && $button_item['icon_heroicons_v2'] !== '') {
                 // FIXME
@@ -68,14 +68,14 @@ class add_buttons extends rcube_plugin
 
             // Font Awesome v5 icon
             // FIXME <i> element is disabled with mobile view
-            if (isset($button_item['icon_fontawesome_v5']) && $button_item['icon_fontawesome_v5'] !== '') {
+            } elseif (isset($button_item['icon_fontawesome_v5']) && $button_item['icon_fontawesome_v5'] !== '') {
                 if ($included_fontawesome_v5 === false) {
                     // FIXME this is just a temporary resource during development / PoC. It has
                     //       to be replaced with a local copy if we are sure to use FA this way.
                     $this->include_stylesheet('https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css');
                     $included_fontawesome_v5 = true;
                 }
-                $content = '<i class="' . $button_item['icon_fontawesome_v5'] . '"></i> ' . $content;
+                $content = '<i class="' . $button_item['icon_fontawesome_v5'] . '"></i>';
 
             // Font Awesome v6 icon
             // FIXME <i> element is disabled in mobile view
@@ -86,7 +86,7 @@ class add_buttons extends rcube_plugin
                     $this->include_stylesheet('https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css');
                     $included_fontawesome_v6 = true;
                 }
-                $content = '<i class="' . $button_item['icon_fontawesome_v6'] . '"></i> ' . $content;
+                $content = '<i class="' . $button_item['icon_fontawesome_v6'] . '"></i>';
             }
 
             // https://docs.roundcube.net/doc/phpdoc/classes/rcube_plugin.html#method_add_button
@@ -98,9 +98,9 @@ class add_buttons extends rcube_plugin
                     'content' => $content,
                     'href' => $button_item['href'],
                     'target' => $button_item['target'],
-                    'class' => 'add_button',
+                    'class' => 'add_buttons',
                     'classSel' => 'selected',
-                    'innerclass' => 'inner',
+                    'innerclass' => '', // FIXME 'inner' for $button_item['label']
                     'onclick' => $button_item['onclick'],
                 ),
                 // target container to add the button to
--- config.inc.php
+++ config.inc.php
@@ -13,20 +13,27 @@
 
 $config['add_buttons'] = array(
 
+    // FA5 is only half the size of FA6, use if possible.
     array(
-        // mandatory
-        //'type' => 'link',
-        //'container' => 'taskbar',
         'href' => 'https://foundata.com/',
         'target' => '_blank',
         'label' => 'foundata',
-        // FIXME To be implemented: 'icon_inline' => '<svg ...',
-        // FA5 is only half the size of FA6, use if possible.
         'icon_fontawesome_v5' => 'fas fa-external-link-alt',
-        //'icon_fontawesome_v6' => 'fa-solid fa-up-right-from-square',
-        // FIXME To be implemented: 'icon_heroicons_v2' => 'foobar.svg',
     ),
 
+    array(
+        'href' => 'https://foundata.com/',
+        'target' => '_blank',
+        'label' => 'foundata',
+        'icon_fontawesome_v6' => 'fa-solid fa-up-right-from-square',
+    ),
+
+    array(
+        'href' => 'https://foundata.com/',
+        'target' => '_blank',
+        'label' => 'foundata',
+        'icon_inline' => '<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" d="M13.19 8.688a4.5 4.5 0 0 1 1.242 7.244l-4.5 4.5a4.5 4.5 0 0 1-6.364-6.364l1.757-1.757m13.35-.622 1.757-1.757a4.5 4.5 0 0 0-6.364-6.364l-4.5 4.5a4.5 4.5 0 0 0 1.242 7.244" /></svg>',
+    ),
 );
 
 
